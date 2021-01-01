497300691ae0e1bcafa33c897a4c112f01549543
Himanshu Jain <hjmanojain.8@gmail.com>
2021-01-01 13:22:04 +0530

    removed express

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const express = require("express");
-const app = express();
-var server = require("http").createServer(app);
+var server = require("http").createServer();
 
-const io = require("socket.io")(server);
-// Register Socket Connection Listener:
-require("./socket/index")(io);
-
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
 });
 
-server.listen(8080, "localhost", () => {
+server.listen(8080, () => {
   console.log("Listening on Port 8080");
 });
+
+// Register Socket Connection Listener:
+require("./socket/index")(io);
 
 module.exports.io = io;
